8e994d7f2cad43c3da3be6a105589b8cc5b75869
Hoàng Thành <hoangthanh5264@gmail.com>
2025-04-22 11:43:40 +0700

    setup tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <>
-      <h1 className="text-3xl text-white">Welcome to Next.js!</h1>
+      <h1 className="h1-bold text-primary-500">Welcome to Next.js!</h1>
       <h1 className="">Welcome to Next.js!</h1>
     </>
   );
