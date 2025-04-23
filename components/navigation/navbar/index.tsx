3dc4cda577f71ff666a0fe47daa4164ac29ca7dc
Hoàng Thành <hoangthanh5264@gmail.com>
2025-04-23 21:46:51 +0700

    set up Light&Dark Theme

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
@@ -17,7 +18,9 @@ const Navbar = () => {
         </p>
       </Link>
       <p>Search</p>
-      <div className="flex-between">Theme</div>
+      <div className="flex-between">
+        <Theme />
+      </div>
     </nav>
   );
 };
