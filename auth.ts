873ecfc9382763a92ebd2c4913aa3c2aabf01971
Hoàng Thành <hoangthanh5264@gmail.com>
2025-04-24 20:24:34 +0700

    Login with Google

--- auth.ts
+++ auth.ts
@@ -3,11 +3,5 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    Google,
-  ],
+  providers: [GitHub, Google],
 });
